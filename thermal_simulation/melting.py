1f396ce8662c048a80f2185f9791b75704161805
Olof <olof.woxenius@telia.com>
2024-11-06 13:43:43 +0100

    achieved (some) stability, wrote on the README

--- thermal_simulation/melting.py
+++ thermal_simulation/melting.py
@@ -5,13 +5,13 @@ from scipy.sparse.linalg import spsolve
 import matplotlib.pyplot as plt
 
 def initialize_conductances(knn_distances):
-    k_0 = 0.001  # base thermal conductivity
+    k_0 = 0.0005  # base thermal conductivity
     conductances = k_0 / knn_distances
     return conductances
 
 def melting(points_per_second=1000, pixels_per_m=2000, k=4):
     dt = 1 / points_per_second
-    T_0 = 300  # Ambient temperature in K
+    T_0 = 300  #System temperature
 
     knn_indices, knn_distances, pixel_coords, point_labels, T = melting_setup(k, T_0, pixels_per_m)
     num_nodes = len(knn_indices)
@@ -19,41 +19,44 @@ def melting(points_per_second=1000, pixels_per_m=2000, k=4):
     
     laplacian = construct_laplacian_matrix(knn_indices, conductances)
 
-    # Radiative loss over all nodes
-    epsilon = 0.6  # Emissivity
-    sigma = 5.67e-8  # Stefan-Boltzmann constant
-    linearized_radiative_coeff = 4 * sigma * epsilon * T_0 ** 3
-    radiative_matrix = diags(linearized_radiative_coeff * np.ones(num_nodes))
+    #Radiation loss over all nodes
+    epsilon = 0.5  #Emissivity
+    sigma = 5.67e-8  #Stefan-Boltzmann constant
+    h_rad = 4 * sigma * epsilon * T_0 ** 3
 
-    # Convective (conductive) loss at edges
+    #Conductive loss at edges
     edge_nodes = (point_labels == 2).astype(float)
     internal_nodes = np.where(point_labels != 2)[0]
-    h_edge = 10 # Heat transfer coefficient at edges, adjust as needed
-    convective_coeff = h_edge * edge_nodes
-    convective_matrix = diags(convective_coeff)
+    h_edge = 0.2 #heat transfer coefficient
+    h_conv = h_edge * edge_nodes
 
-    # Combine system matrix
-    A = laplacian + radiative_matrix + convective_matrix
+    h_total = h_rad + h_conv
+    b = h_total * T_0
+    heat_loss_matrix = diags(h_total)
 
-    # Construct Crank-Nicolson matrices
+    #Combined matrix
+    A = laplacian + heat_loss_matrix
+
+    #Crank-Nicholson matrices
     I = identity(num_nodes)
     lhs_matrix = I + (dt / 2) * A
     rhs_matrix = I - (dt / 2) * A
 
-    # Time-stepping parameters
-    Q_melt = 1000  # No heat input in this scenario
+    Q_melt = 100
     melting_nodes = np.random.choice(internal_nodes, size=num_nodes, replace=True)
     T_max = []
     variance_list = []
-    simulation_steps = int(0.05 * num_nodes)
+    simulation_steps = int(1 * num_nodes)
 
     for n in range(simulation_steps):
         S = np.zeros(num_nodes)
+        #melt_node = internal_nodes[n]
         melt_node = melting_nodes[n]
         S[melt_node] = Q_melt
+        S_total = S + b
 
         # Right-hand side for Crank-Nicolson
-        rhs = rhs_matrix.dot(T) + dt * S
+        rhs = rhs_matrix.dot(T) + dt * S_total
 
         # Solve the linear system
         T = spsolve(lhs_matrix, rhs)
